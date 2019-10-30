96efa5120f53f4ef2340e20b989fd81f8b84bd48
Salar Moghaddam <salarmgh@gmail.com>
2019-10-30 09:51:32 +0330

    Add xcompmgr requrenment

--- req.sh
+++ req.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y xsel emacs-nox xclip fonts-ibm-plex feh mpv cmus vim firefox git curl i3 xinit xterm tmux openconnect arandr i3lock docker docker-compose
+sudo apt-get update && sudo apt-get install -y xsel emacs-nox xclip fonts-ibm-plex feh mpv cmus vim firefox git curl i3 xinit xterm tmux openconnect arandr i3lock docker docker-compose xcompmgr
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
      echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
