ed570847d7077a4467f4342a9e366a9ab38dabcf
Salar Moghaddam <salar.moghaddam@digikala.com>
2020-02-25 18:03:11 +0330

    Update init

--- init.el
+++ init.el
@@ -1,68 +1,215 @@
+;;; Package intialize
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;;; Install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+
+;; Appearance
+;;; Remove unnecessary components
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-message t)
+
+;;; Display line numbers
+(display-line-numbers-mode 1)
+
+;;; Modeline line and column number
+(line-number-mode 1)
+(column-number-mode 1)
+
+;;; Disable scroll jumps
+(setq scroll-conservatively 100)
+
+
+;; Syntax helpers
+;;; Highlight closing paranteses
+(show-paren-mode 1)
+
+;;; Auto close pair
+(setq electric-pair-pairs '(
+                           (?\{ . ?\})
+                           (?\( . ?\))
+                           (?\[ . ?\])
+                           (?\" . ?\")
+                           ))
+(electric-pair-mode t)
+
+;;; Highlight current line
+(when window-system (global-hl-line-mode t))
+
+
+;; Emacs options
+;;; Disable annoying sounds
+(setq ring-bell-function 'ignore)
+
+;;; delete inside selection when typing
+(delete-selection-mode 1)
+
+;;; Disable tabs
+(setq-default indent-tabs-mode nil)
+
+;;; Rectangle select mode
+(global-set-key (kbd "C-x v") 'rectangle-mark-mode)
+
+;;; Rectangle replace mode
+(global-set-key (kbd "C-x r") 'string-rectangle)
+
+;;; Comment region
+(define-key global-map (kbd "C-x /" 'comment-region)
+
+;;; Uncomment region
+(define-key global-map (kbd "C-x \\") 'uncomment-region)
+
+;;; Delete trailing whitespaces
+(setq require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)  
+
+;;; Buffer navigation
+(global-set-key (kbd "M-.") 'next-buffer)
+(global-set-key (kbd "M-,") 'previous-buffer)
+
+
+;; Emacs backup management
+;;; Disable Emacs backup
+(setq make-backup-file nil)
+(setq auto-save-default nil)
+
+
+;; Aliases
+;;; Shortcut for yes or no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+
+;; Variables
+;;; Ansi term as default terminal emulator
+(defvar term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list term-shell)))
+(ad-activate 'ansi-term)
+(global-set-key (kbd "C-x t") 'ansi-term)
+
+
+;;; Dockerfile support
+(use-package dockerfile-mode
+  :ensure t)
+
+;;; docker-compose support
+(use-package docker-compose-mode
+  :ensure t)
+
+;;; yaml support
+(use-package undo-fu
+  :ensure t)
+(global-set-key (kbd "C-q") 'undo-fu-only-undo)
+(global-set-key (kbd "C-.") 'undo-fu-only-redo)
+
+;;; markdown support
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode)
+  :mode (("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)))
+
+;;; Doom themes
 (use-package doom-themes
   :ensure t)
 
-(use-package ido-vertical-mode
-  :ensure t
-  :init
-  (ido-vertical-mode 1))
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
-
+;;; smex for Smarter command executor
 (use-package smex
   :ensure t
   :init (smex-initialize)
   :bind
   ("M-x" . smex))
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq inhibit-startup-message t)
-
-(setq scroll-conservatively 100)
-
-(setq ring-bell-function 'ignore)
-
-(setq make-backup-file nil)
-(setq auto-save-default nil)
-
-(when window-system (global-hl-line-mode t))
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(defvar term-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list term-shell)))
-(ad-activate 'ansi-term)
-
-(global-set-key (kbd "C-x t") 'ansi-term)
-
+;;; Ido interactive buffer manager
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
 (setq ido-create-new-buffer 'always)
 (setq ido-everywhere t)
 (ido-mode 1)
 
+;;; Ibuffer as buffer manager
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-
+;;; Avy shortcut jump to char
 (use-package avy
   :ensure t
   :bind
   ("M-s" . avy-goto-char))
 
+;;; Highlight color code to it's color
 (use-package rainbow-mode
   :ensure t
   :init (rainbow-mode 1))
 
+;;; Different color for different paranteses
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+    (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+;;; Window switch for ease of window navigation
+(use-package switch-window
+  :ensure t
+  :config
+    (setq switch-window-input-style 'minibuffer)
+    (setq switch-window-increase 4)
+    (setq switch-window-threshold 2)
+    (setq switch-window-shortcut-style 'qwerty)
+    (setq switch-window-qwerty-shortcuts
+        '("a" "s" "d" "f" "j" "k" "l" "i" "o"))
+  :bind
+  ([remap other-window] . switch-window))
+
+;;; Multi marker
+(use-package mark-multiple
+  :ensure t
+  :bind ("C-c n" . 'mark-next-like-this))
+
+(use-package mark-multiple
+  :ensure t
+  :bind ("C-c a" . 'mark-all-like-this))
+
+;;; Expand region
+(use-package expand-region
+  :ensure t
+  :bind ("C-q" . er/expand-region))
+
+;;; Yank history menu
+(use-package popup-kill-ring
+  :ensure t
+  :bind ("M-y" . popup-kill-ring))
+
+;;; Smart search
+(use-package swiper
+  :ensure t
+  :bind ("C-s" . 'swiper))
+
+
+;; Custom functions
+;;; Copy whole line
+(defun daedreth/copy-whole-line ()
+  "Copies a line without regard for cursor position."
+  (interactive)
+  (save-excursion
+    (kill-new
+     (buffer-substring
+      (point-at-bol)
+      (point-at-eol)))))
+(global-set-key (kbd "C-x w") 'daedreth/copy-whole-line)
+
+;;; Change focus on new window
 (defun split-and-follow-hor ()
   (interactive)
   (split-window-below)
@@ -77,26 +224,7 @@
   (other-window 1))
 (global-set-key (kbd "C-x 3") 'split-and-follow-ver)
 
-(use-package switch-window
-  :ensure t
-  :config
-    (setq switch-window-input-style 'minibuffer)
-    (setq switch-window-increase 4)
-    (setq switch-window-threshold 2)
-    (setq switch-window-shortcut-style 'qwerty)
-    (setq switch-window-qwerty-shortcuts
-        '("a" "s" "d" "f" "j" "k" "l" "i" "o"))
-  :bind
-  ([remap other-window] . switch-window))
-
-(setq electric-pair-pairs '(
-                           (?\{ . ?\})
-                           (?\( . ?\))
-                           (?\[ . ?\])
-                           (?\" . ?\")
-                           ))
-(electric-pair-mode t)
-
+;;; Delete inner word
 (defun kill-inner-word ()
   (interactive)
   (forward-char 1)
@@ -104,65 +232,126 @@
   (kill-word 1))
 (global-set-key (kbd "C-c w") 'kill-inner-word)
 
-(defun kill-current-buffer ()
-  "Kills the current buffer."
+;;; Copy to clipboard function
+(defun copy-to-clipboard ()
+"Copies selection to x-clipboard."
+(interactive)
+(if (display-graphic-p)
+    (progn
+      (message "Yanked region to x-clipboard!")
+      (call-interactively 'clipboard-kill-ring-save)
+      )
+  (if (region-active-p)
+      (progn
+        (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+        (message "Yanked region to clipboard!")
+        (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+)
+(global-set-key (kbd "C-x c") 'copy-to-clipboard)
+
+; Re-create ci" ca"...
+(defun seek-backward-to-char (chr)
+  "Seek backwards to a character"
+  (interactive "cSeek back to char: ")
+  (while (not (= (char-after) chr))
+    (forward-char -1)))
+
+(setq char-pairs
+      '(( ?\" . ?\" )
+        ( ?\' . ?\' )
+        ( ?\( . ?\) )
+        ( ?\[ . ?\] )
+        ( ?\{ . ?\} )
+        ( ?<  . ?>  )))
+
+(defun get-char-pair (chr)
+  (let ((result ()))
+    (dolist (x char-pairs)
+      (setq start (car x))
+      (setq end (cdr x))
+      (when (or (= chr start) (= chr end))
+        (setq result x)))
+      result))
+
+(defun get-start-char (chr)
+  (car (get-char-pair chr)))
+(defun get-end-char (chr)
+  (cdr (get-char-pair chr)))
+
+(defun seek-to-matching-char (start end count)
+  (while (> count 0)
+    (if (= (following-char) end)
+        (setq count (- count 1))
+      (if (= (following-char) start)
+          (setq count (+ count 1))))
+    (forward-char 1)))
+
+(defun seek-backward-to-matching-char (start end count)
+  (if (= (following-char) end)
+      (forward-char -1))
+  (while (> count 0)
+    (if (= (following-char) start)
+        (setq count (- count 1))
+      (if (= (following-char) end)
+          (setq count (+ count 1))))
+    (if (> count 0)
+        (forward-char -1))))
+
+(defun delete-between-pair (char)
+  "Delete in between the given pair"
+  (interactive "cDelete between char: ")
+  (seek-backward-to-matching-char (get-start-char char) (get-end-char char) 1)
+  (forward-char 1)
+  (setq mark (point))
+  (seek-to-matching-char (get-start-char char) (get-end-char char) 1)
+  (forward-char -1)
+  (kill-region mark (point)))
+
+(defun delete-all-pair (char)
+  "Delete in between the given pair and the characters"
+  (interactive "cDelete all char: ")
+  (seek-backward-to-matching-char (get-start-char char) (get-end-char char) 1)
+  (setq mark (point))
+  (forward-char 1)
+  (seek-to-matching-char (get-start-char char) (get-end-char char) 1)
+  (kill-region mark (point)))
+
+(global-set-key (kbd "C-c i") 'delete-between-pair)
+(global-set-key (kbd "C-c a") 'delete-all-pair)
+
+;;; vim like open above line
+(defun vi-open-line-above ()
+  "Insert a newline above the current line and put point at beginning."
   (interactive)
-  (kill-buffer (current-buffer)))
-(global-set-key (kbd "C-x k") 'kill-current-buffer)
+  (unless (bolp)
+    (beginning-of-line))
+  (newline)
+  (forward-line -1)
+  (indent-according-to-mode))
 
-(defun daedreth/copy-whole-line ()
-  "Copies a line without regard for cursor position."
+;;; vim like open line below
+(defun vi-open-line-below ()
+  "Insert a newline below the current line and put point at beginning."
   (interactive)
-  (save-excursion
-    (kill-new
-     (buffer-substring
-      (point-at-bol)
-      (point-at-eol)))))
-(global-set-key (kbd "C-x w") 'daedreth/copy-whole-line)
+  (unless (eolp)
+    (end-of-line))
+  (newline-and-indent))
 
-(show-paren-mode 1)
+(defun vi-open-below (&optional abovep)
+  "Insert a newline below the current line and put point at beginning.
+With a prefix argument, insert a newline above the current line."
+  (interactive "P")
+  (vi-open-line-below))
 
-(use-package rainbow-delimiters
-  :ensure t
-  :init
-    (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+(defun vi-open-above (&optional abovep)
+  "Insert a newline below the current line and put point at beginning.
+With a prefix argument, insert a newline above the current line."
+  (interactive "P")
+  (vi-open-line-above))
 
-(use-package dashboard
-  :ensure t
-  :config
-    (dashboard-setup-startup-hook)
-    (setq dashboard-startup-banner "~/.emacs.d/img/dashLogo.png")
-    (setq dashboard-items '((recents  . 5)
-                            (projects . 5)))
-    (setq dashboard-banner-logo-title "Hello Salar!"))
-
-(use-package mark-multiple
-  :ensure t
-  :bind ("C-c n" . 'mark-next-like-this))
-
-(use-package mark-multiple
-  :ensure t
-  :bind ("C-c a" . 'mark-all-like-this))
-
-
-(use-package expand-region
-  :ensure t
-  :bind ("C-q" . er/expand-region))
-
-(use-package popup-kill-ring
-  :ensure t
-  :bind ("M-y" . popup-kill-ring))
-
-(use-package swiper
-  :ensure t
-  :bind ("C-s" . 'swiper))
-
-(line-number-mode 1)
-(column-number-mode 1)
-
-
-;New movie
-(global-visual-line-mode t)
+(define-key global-map [(meta n)] 'vi-open-below)
+(define-key global-map [(meta shift n)] 'vi-open-above)
 
 
 (custom-set-variables
