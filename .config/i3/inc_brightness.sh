b13829a68577a52665e43a8eed7b5b90cbadb1f7
Salar Moghaddam <salarmgh@gmail.com>
2020-07-13 20:24:04 +0430

    Update i3

--- .config/i3/inc_brightness.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-current=$(cat /sys/class/backlight/intel_backlight/brightness)
-echo $(($current + 1000)) > /sys/class/backlight/intel_backlight/brightness
-
--- .config/i3status/status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i3status | while :
-do
-    read line
-    mem_free=$(echo "`cat /proc/meminfo | grep "MemFree" | awk '{print $2}'` / 1024 / 1024" | bc)
-    uptime=$(uptime | cut -d' ' -f2)
-    echo "Uptime: $uptime | Mem: $mem_free""G | $line" || exit 1
-done
